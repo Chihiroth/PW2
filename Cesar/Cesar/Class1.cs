ddde51d82a3bca773fc3bf19155b30a892c6eeee
Chihiroth <123184280+Chihiroth@users.noreply.github.com>
2023-03-04 19:52:37 +0300

    Add files via upload

---
+++ Cesar/Cesar/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cesar
+{
+    class Cesar
+    {
+        private static string alph = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+        private string text;
+        public Cesar(string text)
+        {
+            this.text = text;
+        }
+        public static string Encryption(string text)
+        {
+            var res = new StringBuilder();
+            for (int i = 0; i < text.Length; i++)
+                for (int j = 0; j < alph.Length; j++)
+                    if (text[i] == alph[j]) res.Append(alph[(j + 3) % alph.Length]);
+
+            return res.ToString();
+        }
+        public static string Decryption(string crypt)
+        {
+            var res = new StringBuilder();
+            for (int i = 0; i < crypt.Length; i++)
+                for (int j = 0; j < alph.Length; j++)
+                    if (crypt[i] == alph[j]) res.Append(alph[(j - 3 + alph.Length) % alph.Length]);
+
+            return res.ToString();
+        }
+    }
+}
+
---
+++ Cesar/Cesar/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cesar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите текст для перевода в шифр полностью в нижнем регистре");
+            string text = Convert.ToString(Console.ReadLine());
+            Cesar CaesarCipher = new Cesar(text);
+            Console.WriteLine(Cesar.Encryption(text));
+            string crypt = Cesar.Encryption(text);
+            Console.WriteLine(Cesar.Decryption(crypt));
+            Console.ReadKey(true);
+        }
+    }
+}
